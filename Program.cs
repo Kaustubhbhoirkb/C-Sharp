da532e21a00fdea2ea745a1bf01b56d45c466bd8
Kaustubh Subhash Bhoir <98731432+Kaustubhbhoirkb@users.noreply.github.com>
2022-07-24 18:26:03 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿class Rectangle
+{
+    private int Length;
+    private int Width;
+    private int Area;
+
+
+    public void acceptdata()
+    {
+        Console.WriteLine("Enter Length Of A Rectangle");
+        Length = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter Width Of A Rectangle");
+        Width = Convert.ToInt32(Console.ReadLine());
+    }
+    public void displaydata()
+    {
+        Area = Length * Width;
+        Console.WriteLine("Area Of Rectangle Is : " + Area);
+    }
+}
+
+class Result
+{
+    static void Main(string[] args)
+    {
+        Rectangle R1 = new Rectangle();
+        //o1.Id = 10;
+        //Console.WriteLine(o1.Id);
+        R1.acceptdata();
+        R1.displaydata();
+
+    }
+
+}
